b51289ad19e43ca05a34757c8d10ff98c28908a3
fuguanqi <fuguanqi@hotmail.com>
2022-01-16 07:38:40 +0800

    replace cplex with Lee & Choi(1995)

--- ET_opt.py
+++ ET_opt.py
@@ -83,10 +83,9 @@ def init_ET_global_solution(jobs, problem):
     return et_global_solution
 
 
-def opt_ET(memo_ET, et_global_solution, jobs, problem, last):
+def opt_ET(memoBT, memo_ET, et_global_solution, jobs, problem, last):
     if memo_ET[last].head_last >= 0:
         return memo_ET[last]
-
     if last in et_global_solution.block_lasts:
         i = et_global_solution.block_lasts.index(last)
         head_last = et_global_solution.block_lasts[0]
@@ -102,25 +101,30 @@ def opt_ET(memo_ET, et_global_solution, jobs, problem, last):
         num_idle = i
 
     else:
-        if last < et_global_solution.block_lasts[0]:
-            head_last = last
-            tail_first = 0
-            block_start, block_end, et_penalty, _ = bt.time_block_no_memo(jobs, 0, last, problem)
-            tail_start = block_start
-            tail_end = block_end
-            num_idle = 0
-        else:
-            et_penalty = 0
-            num_idle = 1
-            head_last = et_global_solution.block_lasts[0]
-            for i in range(len(et_global_solution.block_lasts) - 1):
-                et_penalty += et_global_solution.block_objs[i]
-                if last > et_global_solution.block_lasts[i] and last < et_global_solution.block_lasts[i + 1]:
-                    tail_first = et_global_solution.block_lasts[i] + 1
-                    tail_start = et_global_solution.block_starttimes[i + 1]
-                    tail_end = et_global_solution.block_endtimes[i + 1]
-                    break
-                num_idle += 1
+        # if last < et_global_solution.block_lasts[0]:
+        #     print("???????????????")
+        #     head_last = last
+        #     tail_first = 0
+        #     block_start, block_end, et_penalty, cplex_time = bt.time_block_no_memo(jobs, 0, last, problem)
+        #     tail_start = block_start
+        #     tail_end = block_end
+        #     num_idle = 0
+        # else:
+        et_penalty = 0
+        num_idle = 1
+        head_last = et_global_solution.block_lasts[0]
+        for i in range(len(et_global_solution.block_lasts) - 1):
+            et_penalty += et_global_solution.block_objs[i]
+            if last > et_global_solution.block_lasts[i] and last < et_global_solution.block_lasts[i + 1]:
+                tail_first = et_global_solution.block_lasts[i] + 1
+                # tail_start = et_global_solution.block_starttimes[i + 1]
+                # tail_end = et_global_solution.block_endtimes[i + 1]
+                start_UB, end_UB, et_penalty_UB, _ = bt.time_block(memoBT, jobs, tail_first, last, problem)
+                tail_start = start_UB
+                tail_end = end_UB
+                et_penalty += et_penalty_UB
+                break
+            num_idle += 1
 
     memo_ET[last].head_last = head_last
     memo_ET[last].tail_first = tail_first
@@ -132,129 +136,115 @@ def opt_ET(memo_ET, et_global_solution, jobs, problem, last):
     return memo_ET[last]
 
 
-def opt_ET_no_memo(jobs, first, last, problem):
+#
+# def opt_ET_no_memo(jobs, first, last, problem):
+#     para_due_dates = problem.due_dates
+#     para_processing_times = problem.processing_times
+#     para_earliness_penalties = problem.earliness_penalties
+#     para_tardiness_penalties = problem.tardiness_penalties
+#     # Create model
+#     opt_model = cpx.Model(name="Calculate E/T Model")
+#     opt_model.parameters.simplex.tolerances.feasibility = 0.0000001
+#
+#     n = last - first + 1
+#
+#     # Decision parameters
+#     end_times = opt_model.continuous_var_list(n, lb=0, name="end_time_of_job_%s")
+#     earlis = opt_model.continuous_var_list(n, lb=0, name="earliness_of_job_%s")
+#     tardis = opt_model.continuous_var_list(n, lb=0, name="tardiness_of_job_%s")
+#
+#     # constraint
+#     opt_model.add_constraints_(
+#         end_times[i] + para_processing_times[jobs[first + i + 1]] <= end_times[i + 1]
+#         for i in range(n - 1)
+#     )
+#
+#     # constraint
+#     opt_model.add_constraints_(
+#         end_times[i] + earlis[i] - tardis[i] == para_due_dates[jobs[first + i]] for i in range(n)
+#     )
+#
+#     # Objective function
+#     objective_function = opt_model.sum(
+#         earlis[i] * para_earliness_penalties[jobs[first + i]] + tardis[i] * para_tardiness_penalties[jobs[first + i]]
+#         for i in range(n)
+#     )
+#
+#     # minimize objective
+#     opt_model.minimize(objective_function)
+#     opt_model.solve()
+#     head_last = last
+#     tail_first = first
+#     is_head = 1
+#     num_idle = 0
+#     for i in range(n - 1):
+#         end1 = opt_model.solution.get_value("end_time_of_job_" + str(i))
+#         end2 = opt_model.solution.get_value("end_time_of_job_" + str(i + 1))
+#         if round(end1 + para_processing_times[jobs[first + i + 1]], 4) != round(end2, 4):
+#             if is_head == 1:
+#                 head_last = i
+#                 is_head = 0
+#             num_idle += 1
+#
+#     for i in range(n - 1):
+#         end1 = opt_model.solution.get_value("end_time_of_job_" + str(n - 2 - i))
+#         end2 = opt_model.solution.get_value("end_time_of_job_" + str(n - 1 - i))
+#         if round(end1 + para_processing_times[jobs[last - i]], 4) != round(end2, 4):
+#             tail_first = last - i
+#             break
+#
+#     et_penalty = opt_model.solution.get_objective_value()
+#
+#     return head_last, tail_first, et_penalty, num_idle
+
+
+def opt_ET_with_boundary(memoBT, et_global_solution, jobs, last, problem, boundary):
     para_due_dates = problem.due_dates
     para_processing_times = problem.processing_times
     para_earliness_penalties = problem.earliness_penalties
     para_tardiness_penalties = problem.tardiness_penalties
-    # Create model
-    opt_model = cpx.Model(name="Calculate E/T Model")
-    opt_model.parameters.simplex.tolerances.feasibility = 0.0000001
+    if last in et_global_solution.block_lasts:
+        i = et_global_solution.block_lasts.index(last)
+        et_block_lasts = et_global_solution.block_lasts[0:i + 1]
+        et_block_ends = et_global_solution.block_endtimes[0:i + 1]
+        et_block_objs=et_global_solution.block_objs[0:i+1]
 
-    n = last - first + 1
+    else:
+        et_block_lasts = list()
+        et_block_ends = list()
+        et_block_objs=list()
+        if last > et_global_solution.block_lasts[0]:
+            for i in range(len(et_global_solution.block_lasts) - 1):
+                et_block_objs.append(et_global_solution.block_objs[i])
+                et_block_lasts.append(et_global_solution.block_lasts[i])
+                et_block_ends.append(et_global_solution.block_endtimes[i])
+                if last > et_global_solution.block_lasts[i] and last < et_global_solution.block_lasts[i + 1]:
+                    tail_first = et_global_solution.block_lasts[i] + 1
+                    start_UB, end_UB, et_penalty_UB, _ = bt.time_block(memoBT, jobs, tail_first, last, problem)
+                    et_block_ends.append(end_UB)
+                    et_block_lasts.append(last)
+                    et_block_objs.append(et_penalty_UB)
+                    break
 
-    # Decision parameters
-    end_times = opt_model.continuous_var_list(n, lb=0, name="end_time_of_job_%s")
-    earlis = opt_model.continuous_var_list(n, lb=0, name="earliness_of_job_%s")
-    tardis = opt_model.continuous_var_list(n, lb=0, name="tardiness_of_job_%s")
-
-    # constraint
-    opt_model.add_constraints_(
-        end_times[i] + para_processing_times[jobs[first + i + 1]] <= end_times[i + 1]
-        for i in range(n - 1)
-    )
-
-    # constraint
-    opt_model.add_constraints_(
-        end_times[i] + earlis[i] - tardis[i] == para_due_dates[jobs[first + i]] for i in range(n)
-    )
-
-    # Objective function
-    objective_function = opt_model.sum(
-        earlis[i] * para_earliness_penalties[jobs[first + i]] + tardis[i] * para_tardiness_penalties[jobs[first + i]]
-        for i in range(n)
-    )
-
-    # minimize objective
-    opt_model.minimize(objective_function)
-    opt_model.solve()
-    head_last = last
-    tail_first = first
-    is_head = 1
-    num_idle = 0
-    for i in range(n - 1):
-        end1 = opt_model.solution.get_value("end_time_of_job_" + str(i))
-        end2 = opt_model.solution.get_value("end_time_of_job_" + str(i + 1))
-        if round(end1 + para_processing_times[jobs[first + i + 1]], 4) != round(end2, 4):
-            if is_head == 1:
-                head_last = i
-                is_head = 0
-            num_idle += 1
-
-    for i in range(n - 1):
-        end1 = opt_model.solution.get_value("end_time_of_job_" + str(n - 2 - i))
-        end2 = opt_model.solution.get_value("end_time_of_job_" + str(n - 1 - i))
-        if round(end1 + para_processing_times[jobs[last - i]], 4) != round(end2, 4):
-            tail_first = last - i
+    t = boundary
+    i = last
+    tail_penalty = 0
+    while True:
+        tail_penalty += max(0, para_due_dates[jobs[i]] - t) * para_earliness_penalties[jobs[i]] +\
+                        max(0, t -para_due_dates[jobs[i]]) * para_tardiness_penalties[jobs[i]]
+        t = t - para_processing_times[jobs[i]]
+        i=i-1
+        if i==-1:
+            b_penalty=tail_penalty
+            num_idle=0
             break
+        if i in et_block_lasts:
+            b = et_block_lasts.index(i)
+            if et_block_ends[b] < t:
+                b_penalty=sum(et_block_objs[0:b])+tail_penalty
+                num_idle=b+1
+                break
 
-    et_penalty = opt_model.solution.get_objective_value()
-
-    return head_last, tail_first, et_penalty, num_idle
 
 
-def opt_ET_with_boundary(jobs, last, problem, boundary):
-    para_due_dates = problem.due_dates
-    para_processing_times = problem.processing_times
-    para_earliness_penalties = problem.earliness_penalties
-    para_tardiness_penalties = problem.tardiness_penalties
-    # Create model
-    opt_model = cpx.Model(name="Calculate E/T Model")
-    opt_model.parameters.simplex.tolerances.feasibility = 0.0000001
-
-    n = last + 1
-
-    # Decision parameters
-    end_times = opt_model.continuous_var_list(n, lb=0, name="end_time_of_job_%s")
-    earlis = opt_model.continuous_var_list(n, lb=0, name="earliness_of_job_%s")
-    tardis = opt_model.continuous_var_list(n, lb=0, name="tardiness_of_job_%s")
-
-    # constraint
-    opt_model.add_constraints_(
-        end_times[i] + para_processing_times[jobs[i + 1]] <= end_times[i + 1]
-        for i in range(n - 1)
-    )
-
-    # constraint
-    opt_model.add_constraints_(
-        end_times[i] + earlis[i] - tardis[i] == para_due_dates[jobs[i]] for i in range(n)
-    )
-
-    # constraint
-    opt_model.add_constraint_(
-        end_times[last] == boundary
-    )
-
-    # Objective function
-    objective_function = opt_model.sum(
-        earlis[i] * para_earliness_penalties[jobs[i]] + tardis[i] * para_tardiness_penalties[jobs[i]]
-        for i in range(n)
-    )
-
-    # minimize objective
-    opt_model.minimize(objective_function)
-    opt_model.solve()
-    head_last = last
-    tail_first = 0
-    is_head = 1
-    num_idle = 0
-    for i in range(n - 1):
-        end1 = opt_model.solution.get_value("end_time_of_job_" + str(i))
-        end2 = opt_model.solution.get_value("end_time_of_job_" + str(i + 1))
-        if round(end1 + para_processing_times[jobs[i + 1]], 4) != round(end2, 4):
-            if is_head == 1:
-                head_last = i
-                is_head = 0
-            num_idle += 1
-
-    for i in range(n - 1):
-        end1 = opt_model.solution.get_value("end_time_of_job_" + str(n - 2 - i))
-        end2 = opt_model.solution.get_value("end_time_of_job_" + str(n - 1 - i))
-        if round(end1 + para_processing_times[jobs[last - i]], 4) != round(end2, 4):
-            tail_first = last - i
-            break
-
-    et_penalty = opt_model.solution.get_objective_value()
-    end_time = opt_model.solution.get_value("end_time_of_job_" + str(last))
-
-    return et_penalty, num_idle
+    return b_penalty, num_idle
--- blockTiming.py
+++ blockTiming.py
@@ -9,6 +9,12 @@ class BT_solution:
         self.block_end = -1
         self.et_penalty = -1
 
+class BP:
+    def __init__(self):
+        self.job = -1
+        self.tard=0
+        self.delta_slope=0
+
 
 def init_BT_memo(jobs, para_due_dates, para_processing_times):
     n = len(jobs)
@@ -25,112 +31,186 @@ def init_BT_memo(jobs, para_due_dates, para_processing_times):
     return memo_BT
 
 
-# jobs as list
-def time_block(memoBT, jobs, first, last, problem):
+# # jobs as list
+# def time_block(memoBT, jobs, first, last, problem):
+#     if memoBT[first][last].block_start >= 0:
+#         return memoBT[first][last].block_start, memoBT[first][last].block_end, memoBT[first][last].et_penalty, 0
+#     block_start = 0
+#     block_end = 0
+#     et_penalty = utils.BIG_NUMBER
+#     for i in range(first, last + 1):
+#         tmp_block_start, tmp_block_end, tmp_et = cal_block_ET(jobs, first, last, i, problem)
+#         if tmp_et < et_penalty:
+#             block_end = tmp_block_end
+#             block_start = tmp_block_start
+#             et_penalty = tmp_et
+#
+#     # # Create model
+#     #
+#     # opt_model = cpx.Model(name="Block Timing Model")
+#     #
+#     # opt_model.parameters.simplex.tolerances.feasibility = 0.0000001
+#     #
+#     # n = last - first + 1
+#     # # Decision parameters
+#     # end_times = opt_model.continuous_var_list(n, lb=0, ub=utils.BIG_NUMBER, name="end_time_of_job_%s")
+#     # earlis = opt_model.continuous_var_list(n, lb=0, ub=utils.BIG_NUMBER, name="earliness_of_job_%s")
+#     # tardis = opt_model.continuous_var_list(n, lb=0, ub=utils.BIG_NUMBER, name="tardiness_of_job_%s")
+#     #
+#     # # constraint
+#     # opt_model.add_constraints_(
+#     #     end_times[i] + para_processing_times[jobs[first + i + 1]] == end_times[i + 1] for i in range(n - 1)
+#     # )
+#     #
+#     # # constraint
+#     # opt_model.add_constraints_(
+#     #     end_times[i] + earlis[i] - tardis[i] == para_due_dates[jobs[first + i]] for i in range(n)
+#     # )
+#     #
+#     # # Objective function
+#     # objective_function = opt_model.sum(
+#     #     earlis[i] * para_earliness_penalties[jobs[first + i]] + tardis[i] * para_tardiness_penalties[jobs[first + i]]
+#     #     for i in range(n)
+#     # )
+#     #
+#     # # minimize objective
+#     # start = time.process_time()
+#     # opt_model.minimize(objective_function)
+#     # # print("**************** Block Timing ****************")
+#     # # opt_model.print_information()
+#     # end = time.process_time()
+#     # opt_model.solve()
+#     #
+#     # cplex_time = end - start
+#     #
+#     # block_end = opt_model.solution.get_value("end_time_of_job_" + str(n - 1))
+#     # block_start = opt_model.solution.get_value("end_time_of_job_0") - para_processing_times[jobs[first]]
+#     # et_penalty = opt_model.solution.get_objective_value()
+#
+#     memoBT[first][last].block_start = block_start
+#     memoBT[first][last].block_end = block_end
+#     memoBT[first][last].et_penalty = et_penalty
+#
+#     # print("block start:", block_start)
+#     # print("block end:", block_end)
+#     # print("E/T penalty:", et_penalty)
+#     # print("**************** Block Timing Finished ****************")
+#     return block_start, block_end, et_penalty, 0
+
+
+def cal_block_ET(jobs, first, last, j_due, problem):
     para_due_dates = problem.due_dates
     para_processing_times = problem.processing_times
     para_earliness_penalties = problem.earliness_penalties
     para_tardiness_penalties = problem.tardiness_penalties
+    ci = para_due_dates[jobs[j_due]]
+    et = 0
+    for i in range(j_due - 1, first - 1, -1):
+        ci = ci - para_processing_times[jobs[i + 1]]
+        et = et + max(0, (para_due_dates[jobs[i]] - ci)) * para_earliness_penalties[jobs[i]] + max(0, (
+                ci - para_due_dates[jobs[i]])) * para_tardiness_penalties[jobs[i]]
+    block_start = ci - para_processing_times[jobs[first]]
+
+    ci = para_due_dates[jobs[j_due]]
+    for i in range(j_due + 1, last + 1):
+        ci = ci + para_processing_times[jobs[i]]
+        et = et + max(0, (para_due_dates[jobs[i]] - ci)) * para_earliness_penalties[jobs[i]] + max(0, (
+                ci - para_due_dates[jobs[i]])) * para_tardiness_penalties[jobs[i]]
+    block_end = ci
+    return block_start, block_end, et
+
+
+def time_block(memoBT, jobs, first, last, problem): #Lee & Choi
     if memoBT[first][last].block_start >= 0:
-        return memoBT[first][last].block_start, memoBT[first][last].block_end, memoBT[first][last].et_penalty,0
-
-    # Create model
-    opt_model = cpx.Model(name="Block Timing Model")
-    opt_model.parameters.simplex.tolerances.feasibility = 0.0000001
-
-    n = last - first + 1
-    # Decision parameters
-    end_times = opt_model.continuous_var_list(n, lb=0, ub=utils.BIG_NUMBER, name="end_time_of_job_%s")
-    earlis = opt_model.continuous_var_list(n, lb=0, ub=utils.BIG_NUMBER, name="earliness_of_job_%s")
-    tardis = opt_model.continuous_var_list(n, lb=0, ub=utils.BIG_NUMBER, name="tardiness_of_job_%s")
-
-    # constraint
-    opt_model.add_constraints_(
-        end_times[i] + para_processing_times[jobs[first + i + 1]] == end_times[i + 1] for i in range(n - 1)
-    )
-
-    # constraint
-    opt_model.add_constraints_(
-        end_times[i] + earlis[i] - tardis[i] == para_due_dates[jobs[first + i]] for i in range(n)
-    )
-
-    # Objective function
-    objective_function = opt_model.sum(
-        earlis[i] * para_earliness_penalties[jobs[first + i]] + tardis[i] * para_tardiness_penalties[jobs[first + i]]
-        for i in range(n)
-    )
-
-    # minimize objective
-    opt_model.minimize(objective_function)
-    # print("**************** Block Timing ****************")
-    # opt_model.print_information()
-    start = time.process_time()
-    opt_model.solve()
-    end = time.process_time()
-    cplex_time = end - start
-
-    block_end = opt_model.solution.get_value("end_time_of_job_" + str(n - 1))
-    block_start = opt_model.solution.get_value("end_time_of_job_0") - para_processing_times[jobs[first]]
-    et_penalty = opt_model.solution.get_objective_value()
-
-    memoBT[first][last].block_start = block_start
-    memoBT[first][last].block_end = block_end
-    memoBT[first][last].et_penalty = et_penalty
-
-    # print("block start:", block_start)
-    # print("block end:", block_end)
-    # print("E/T penalty:", et_penalty)
-    # print("**************** Block Timing Finished ****************")
-    return block_start, block_end, et_penalty,cplex_time
-
-def time_block_no_memo(jobs, first, last, problem):
+        return memoBT[first][last].block_start, memoBT[first][last].block_end, memoBT[first][last].et_penalty, 0
+    breakpoints=[BP() for i in range(last-first+1)]
     para_due_dates = problem.due_dates
     para_processing_times = problem.processing_times
     para_earliness_penalties = problem.earliness_penalties
     para_tardiness_penalties = problem.tardiness_penalties
+    ci=para_due_dates[jobs[first]]-para_processing_times[jobs[first]] #ET function of c0
+    for i in range(last-first+1):
+        ci = ci + para_processing_times[jobs[first+i]]
+        breakpoints[i].job=first+i
+        breakpoints[i].tard=ci-para_due_dates[jobs[first+i]]
+        breakpoints[i].delta_slope=para_tardiness_penalties[jobs[first+i]]+para_earliness_penalties[jobs[first+i]]
+    breakpoints.sort(key=lambda x: x.tard,reverse=True)
+    slope=0
+    for i in range(first,last+1):
+        slope+=para_earliness_penalties[jobs[i]]
+    i=-1
+    while slope>=0:
+        i+=1
+        slope=slope-breakpoints[i].delta_slope
+    j_due=breakpoints[i].job
+    ci = para_due_dates[jobs[j_due]]
+    et = 0
+    for i in range(j_due - 1, first - 1, -1):
+        ci = ci - para_processing_times[jobs[i + 1]]
+        et = et + max(0, (para_due_dates[jobs[i]] - ci)) * para_earliness_penalties[jobs[i]] + max(0, (
+                ci - para_due_dates[jobs[i]])) * para_tardiness_penalties[jobs[i]]
+    block_start = ci - para_processing_times[jobs[first]]
 
-    # Create model
-    opt_model = cpx.Model(name="Block Timing Model")
-    opt_model.parameters.simplex.tolerances.feasibility = 0.0000001
+    ci = para_due_dates[jobs[j_due]]
+    for i in range(j_due + 1, last + 1):
+        ci = ci + para_processing_times[jobs[i]]
+        et = et + max(0, (para_due_dates[jobs[i]] - ci)) * para_earliness_penalties[jobs[i]] + max(0, (
+                ci - para_due_dates[jobs[i]])) * para_tardiness_penalties[jobs[i]]
+    block_end = ci
 
-    n = last - first + 1
-    # Decision parameters
-    end_times = opt_model.continuous_var_list(n, lb=0, ub=utils.BIG_NUMBER, name="end_time_of_job_%s")
-    earlis = opt_model.continuous_var_list(n, lb=0, ub=utils.BIG_NUMBER, name="earliness_of_job_%s")
-    tardis = opt_model.continuous_var_list(n, lb=0, ub=utils.BIG_NUMBER, name="tardiness_of_job_%s")
+    return block_start, block_end, et, 0
 
-    # constraint
-    opt_model.add_constraints_(
-        end_times[i] + para_processing_times[jobs[first + i + 1]] == end_times[i + 1] for i in range(n - 1)
-    )
 
-    # constraint
-    opt_model.add_constraints_(
-        end_times[i] + earlis[i] - tardis[i] == para_due_dates[jobs[first + i]] for i in range(n)
-    )
-
-    # Objective function
-    objective_function = opt_model.sum(
-        earlis[i] * para_earliness_penalties[jobs[first + i]] + tardis[i] * para_tardiness_penalties[jobs[first + i]]
-        for i in range(n)
-    )
-
-    # minimize objective
-    opt_model.minimize(objective_function)
-    # print("**************** Block Timing ****************")
-    # opt_model.print_information()
-    start = time.process_time()
-    opt_model.solve()
-    end = time.process_time()
-    cplex_time = end - start
-
-    block_end = opt_model.solution.get_value("end_time_of_job_" + str(n - 1))
-    block_start = opt_model.solution.get_value("end_time_of_job_0") - para_processing_times[jobs[first]]
-    et_penalty = opt_model.solution.get_objective_value()
-
-    # print("block start:", block_start)
-    # print("block end:", block_end)
-    # print("E/T penalty:", et_penalty)
-    # print("**************** Block Timing Finished ****************")
-    return block_start, block_end, et_penalty,cplex_time
-
+# def time_block_no_memo(jobs, first, last, problem):
+#     para_due_dates = problem.due_dates
+#     para_processing_times = problem.processing_times
+#     para_earliness_penalties = problem.earliness_penalties
+#     para_tardiness_penalties = problem.tardiness_penalties
+#
+#     # Create model
+#
+#     opt_model = cpx.Model(name="Block Timing Model")
+#
+#     opt_model.parameters.simplex.tolerances.feasibility = 0.0000001
+#
+#     n = last - first + 1
+#     # Decision parameters
+#     end_times = opt_model.continuous_var_list(n, lb=0, ub=utils.BIG_NUMBER, name="end_time_of_job_%s")
+#     earlis = opt_model.continuous_var_list(n, lb=0, ub=utils.BIG_NUMBER, name="earliness_of_job_%s")
+#     tardis = opt_model.continuous_var_list(n, lb=0, ub=utils.BIG_NUMBER, name="tardiness_of_job_%s")
+#
+#     # constraint
+#     opt_model.add_constraints_(
+#         end_times[i] + para_processing_times[jobs[first + i + 1]] == end_times[i + 1] for i in range(n - 1)
+#     )
+#
+#     # constraint
+#     opt_model.add_constraints_(
+#         end_times[i] + earlis[i] - tardis[i] == para_due_dates[jobs[first + i]] for i in range(n)
+#     )
+#
+#     # Objective function
+#     objective_function = opt_model.sum(
+#         earlis[i] * para_earliness_penalties[jobs[first + i]] + tardis[i] * para_tardiness_penalties[jobs[first + i]]
+#         for i in range(n)
+#     )
+#
+#     # minimize objective
+#     start = time.process_time()
+#     opt_model.minimize(objective_function)
+#     # print("**************** Block Timing ****************")
+#     # opt_model.print_information()
+#     end = time.process_time()
+#     opt_model.solve()
+#
+#     cplex_time = end - start
+#
+#     block_end = opt_model.solution.get_value("end_time_of_job_" + str(n - 1))
+#     block_start = opt_model.solution.get_value("end_time_of_job_0") - para_processing_times[jobs[first]]
+#     et_penalty = opt_model.solution.get_objective_value()
+#
+#     # print("block start:", block_start)
+#     # print("block end:", block_end)
+#     # print("E/T penalty:", et_penalty)
+#     # print("**************** Block Timing Finished ****************")
+#     return block_start, block_end, et_penalty, cplex_time
